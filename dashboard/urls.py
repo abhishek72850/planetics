1e2878b3f65d8b6dda46d04e6f19a2029270ded0
Abhishek kumar <alexkay72850@gmail.com>
2019-12-31 02:29:17 +0530

    Static added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
 
 urlpatterns = [
-    path('', views.dashboard)
+    path('', views.dashboard),
+    (r'^static/(?P<path>.*)$', 'django.contrib.staticfiles.views', {'document_root': settings.STATIC_ROOT}),
 ]
