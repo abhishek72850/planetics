b457e30ce90ca911d3a67ff6a0542e23d5bd415f
Abhishek kumar <alexkay72850@gmail.com>
2019-12-31 02:22:47 +0530

    Static root updated

--- planetics/settings.py
+++ planetics/settings.py
@@ -137,4 +137,4 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'planetics/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'dashboard/static')]
