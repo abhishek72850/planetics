db8bccdaeeb818fd8604554952ce75c919ecdee8
Abhishek kumar <alexkay72850@gmail.com>
2019-12-31 02:43:47 +0530

    Static root updated

--- planetics/settings.py
+++ planetics/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 # }
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,6 +136,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'dashboard/static')]
