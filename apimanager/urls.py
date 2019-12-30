bbb37915c6fc3444a492c2a284af6e9f9940f679
Abhishek kumar <alexkay72850@gmail.com>
2019-12-31 03:02:49 +0530

    Static added

--- apimanager/urls.py
+++ apimanager/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('fetch/keyword/', views.KeywordAnalysis.as_view()),
     path('fetch/sentiment/', views.SentimentAnalysis.as_view()),
     path('fetch/tone/', views.ToneAnalysis.as_view()),
-    path('fetch/visuals/', views.VisualAnalysis.as_view())
+    path('fetch/visuals/', views.VisualAnalysis.as_view()),
+    (r'^static/(?P<path>.*)$', 'django.contrib.staticfiles.views', {'document_root': settings.STATIC_ROOT}),
 ]
