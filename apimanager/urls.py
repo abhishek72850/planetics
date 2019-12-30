6e3ec03288f23188ae5a0b493a816534032ca63f
Abhishek kumar <alexkay72850@gmail.com>
2019-12-31 03:05:21 +0530

    Static added

--- apimanager/urls.py
+++ apimanager/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
 
 urlpatterns = [
     path('fetch/post/', views.SocialFetcher.as_view()),
